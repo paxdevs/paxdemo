c75c0ccbe06cbf92d6ba3fd66b120ac659586568
Jie Feng <hjf3108@gmail.com>
2024-09-19 21:29:39 +0800

    Add printer test

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include "pax/keypad.h"
 #include "pax/touchscreen.h"
 #include "pax/sound.h"
+#include "pax/printer.h"
 
+#include <algorithm>
 #include <fcntl.h>
+#include <future>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/time.h>
@@ -14,13 +17,14 @@
 
 void audio_thread();
 
-__attribute__((constructor))
-int app_start()
+__attribute__((constructor)) int app_start()
 {
     PAXFramebuffer fb;
     PAXKeypad kp;
     PAXTouchscreen ts;
-    //PAXSound snd;
+    // PAXSound snd;
+    PAXPrinter pr;
+    PAXPrinterCanvas canvas(288);
 
     // Map the pixel data into memory
     int videofd = open("/data/app/MAINAPP/lib/video.frames", O_RDONLY);
@@ -31,8 +35,8 @@ int app_start()
     // mmap the video file
     struct stat st;
     fstat(videofd, &st);
-    uint8_t *video = (uint8_t*)mmap(0, st.st_size, PROT_READ, MAP_PRIVATE, videofd, 0);
-    if (video == (uint8_t*)-1)
+    uint8_t *video = (uint8_t *)mmap(0, st.st_size, PROT_READ, MAP_PRIVATE, videofd, 0);
+    if (video == (uint8_t *)-1)
     {
         return 0;
     }
@@ -47,36 +51,61 @@ int app_start()
     struct timeval start_ts;
     gettimeofday(&start_ts, 0);
     uint64_t target_us = 1000000 / 60;
-    while(ptr < st.st_size) {
+
+    while (ptr < st.st_size)
+    {
         // Frame is run-length encoded, msb is pixel value, rest of the byte is pixel count
         uint8_t cur_pixel_group = video[ptr];
         uint8_t cur_pixel = cur_pixel_group >> 7;
         uint8_t cur_pixel_cnt = cur_pixel_group & 0x7F;
-        for(int i = 0;i < cur_pixel_cnt;i++) {
+        for (int i = 0; i < cur_pixel_cnt; i++)
+        {
             // Screen is inverted, so we need to draw from the bottom up
             int x = cur_pixels % 240;
             int y = cur_pixels / 240;
-            fb.drawPixel(x, 320-70-y, cur_pixel * 0xFF, cur_pixel * 0xFF, cur_pixel * 0xFF);
+            fb.drawPixel(x, 320 - 70 - y, cur_pixel * 0xFF, cur_pixel * 0xFF, cur_pixel * 0xFF);
             cur_pixels++;
         }
         ptr++;
         // Once we've drawn a full frame, sleep to maintain 60fps
-        if (cur_pixels == 240*180) {
-            
-            // Allow termination with the ESC key
-            if(kp.getKey() == KEY_ESC) {
+        if (cur_pixels == 240 * 180)
+        {
+
+            KeyCode pressed = kp.getKey();
+
+            if (pressed == KEY_ESC)
+            {
                 break;
+            }
+            if (pressed == KEY_1)
+            {
+                // Scale the image using nearest neighbor
+                for (int j = 0; j < 384; j++)
+                {
+                    for (int i = 0; i < 288; i++)
+                    {
+                        canvas.setPixel(j, i, fb.pixel(j * 240 / 384, 320 - 70 - i * 180 / 288) == 0);
+                    }
+                }
+                // Print the image asynchronously
+                std::thread([&pr, &canvas]
+                            { pr.print(canvas); })
+                    .detach();
             }
 
             auto ev = ts.getTouchEvent();
             auto [x, y, p] = ev;
-            if (x != -1) {
-                for(int i = 0;i < 320;i++) {
+            if (x != -1)
+            {
+                for (int i = 0; i < 320; i++)
+                {
                     fb.drawPixel(x, i, 0xFF, 0, 0xFF);
                 }
             }
-            if (y != -1) {
-                for(int i = 0;i < 240;i++) {
+            if (y != -1)
+            {
+                for (int i = 0; i < 240; i++)
+                {
                     fb.drawPixel(i, y, 0, 0xFF, 0xFF);
                 }
             }
@@ -84,7 +113,8 @@ int app_start()
             struct timeval end_ts;
             gettimeofday(&end_ts, 0);
             uint64_t elapsed_us = (uint64_t)(end_ts.tv_sec - start_ts.tv_sec) * 1000000 + (end_ts.tv_usec - start_ts.tv_usec);
-            if (elapsed_us < target_us) {
+            if (elapsed_us < target_us)
+            {
                 struct timespec ts;
                 ts.tv_sec = 0;
                 ts.tv_nsec = (target_us - elapsed_us) * 1000;
