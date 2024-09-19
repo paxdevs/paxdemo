7534bc96a3ad2ea088449b8dde3d1b5d15d46a3f
Jie Feng <hjf3108@gmail.com>
2024-09-19 23:53:27 +0800

    Fix audio playback

--- audio.cpp
+++ audio.cpp
@@ -2,27 +2,28 @@
 #include "pax/sound.h"
 #include "opusfile.h"
 
+opus_int16 pcm[5760*16];
+opus_int16 pcm_play[5760*16];
 void audio_thread() {
     PAXSound snd;
-    snd.setSamplerate(24000);
     OggOpusFile *of = op_open_file("/data/app/MAINAPP/lib/audio.opus", 0);
     if (of == NULL) {
         return;
     }
     int ret;
-    opus_int16 pcm[5760];
     while (1) {
-        ret = op_read(of, pcm, 5760, NULL);
+        ret = op_read(of, pcm, 5760*8, 0);
         if (ret < 0) {
             break;
         }
         for(int i = 0;i < ret;i++) {
             pcm[i] = pcm[i] / 4;
+            pcm_play[i*2] = pcm[i];
+            pcm_play[i*2+1] = pcm[i];
         }
-
-        void* ptr = pcm;
+        void* ptr = pcm_play;
         while (ret > 0) {
-            int written = snd.playSound(ptr, ret * 2);
+            int written = snd.playSound(ptr, ret * 4);
             ret -= written / 2;
             ptr = (void*)((uint8_t*)ptr + written);
         }
